be267f9a467f811a514f9236223c463a4c98a3f7
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-07-11 13:12:02 +0530

    Add files via upload

---
+++ Hands-on/Positivenegetive.java
@@ -0,0 +1,22 @@
+package Loops;
+import java.util.*;
+public class Positivenegetive 
+{
+
+	public static void main(String[] args) 
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a value");
+		a=s.nextInt();	
+		if (a>=0)
+		{
+			System.out.println("Number is positive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+		}
+
+}
