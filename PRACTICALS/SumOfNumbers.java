aab44ea6d5b0e79556057a77ec5117c57622520f
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-07-13 13:24:56 +0530

    Add files via upload

---
+++ PRACTICALS/SumOfNumbers.java
@@ -0,0 +1,33 @@
+package Loops;
+
+import java.util.Scanner;
+//Program that calculates and prints the sum of all numbers from user input
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		
+		//Program that calculates and prints the sum of all numbers from user input
+		
+	 	//Scanner class object
+        Scanner s = new Scanner(System.in);
+        
+        //input from user
+        System.out.print("Enter the number of values to sum: ");
+        
+        //integer input
+        int count = s.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= count; i++) 
+        {
+            System.out.print("Enter value #" + i + ": ");
+            int value = s.nextInt();
+            sum += value;
+        }
+
+        System.out.println("The sum of all " + count + " numbers is: " + sum);
+
+        s.close();
+    
+	}
+
+}
