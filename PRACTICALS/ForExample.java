332c656f61c3978e0cb7d3429c6e1125712d2da6
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-07-13 13:24:17 +0530

    Add files via upload

---
+++ PRACTICALS/ForExample.java
@@ -0,0 +1,14 @@
+package Loops;
+//Simple For Loop example
+public class ForExample {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Values of i is :"+ i);
+		}
+
+
+	}
+
+}
