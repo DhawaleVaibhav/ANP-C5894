ced019f95c3282800c2b9b563e6a9fc0418b3fdd
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-07-13 13:27:03 +0530

    Add files via upload

---
+++ PRACTICALS/WhileLoopExample.java
@@ -0,0 +1,23 @@
+package Loops;
+//Simple while loop program
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		//Simple while loop program
+		//initialized x to 10
+				int x=10;
+				
+				//condition
+				while(x<20)
+				{
+					System.out.println("Value of x:" +x);
+					x++;
+				}
+
+			}
+
+		
+
+	
+
+}
