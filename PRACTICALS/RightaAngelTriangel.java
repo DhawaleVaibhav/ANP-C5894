a16adbf4551040effa264dbcee454b5986970e9b
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-07-13 13:25:54 +0530

    Add files via upload

---
+++ PRACTICALS/RightaAngelTriangel.java
@@ -0,0 +1,22 @@
+package Loops;
+import java.util.*;
+public class RightaAngelTriangel {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows for the triangle: ");
+        int rows = s.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        s.close();
+	}
+
+}
