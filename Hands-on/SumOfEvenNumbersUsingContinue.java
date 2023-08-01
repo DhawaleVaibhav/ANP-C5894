dfc144fae36dd9b566e283519ba71028ef14e787
DhawaleVaibhav <90079790+DhawaleVaibhav@users.noreply.github.com>
2023-08-01 20:47:57 +0530

    Add files via upload

--- Hands-on/SumOfEvenNumbersUsingContinue.java
+++ Hands-on/SumOfEvenNumbersUsingContinue.java
@@ -1,13 +1,8 @@
 package Loops;
 /*
- 1 - The continue statement in Java is used to skip the remaining code
- within a loop iteration and proceed to the next iteration.
- It is primarily used to control the flow of a loop and 
- selectively skip certain iterations based on a condition.
- 
- 2 - Ques.Write a the program that asks the user for the number of iterations 
- they want to perform. It then uses a for loop to iterate from 1 
- to the specified number of iterations.
+Write a program that prompts the user to enter a series of integers.
+ The program should calculate the sum of all the even numbers entered,
+skipping any negative numbers. Use the continue statement to skip negative numbers.
  */
 import java.util.Scanner;
 public class SumOfEvenNumbersUsingContinue 
@@ -26,21 +21,21 @@ public class SumOfEvenNumbersUsingContinue
     	  number=sc.nextInt();
     	      	  for(int i=1;i<=1;i++)
     	  {
-    		  //to reject number divisible by 3 (condition for 2 question)
-    		  if ((number % 3 ==0) || (number<0))
-    			  
+    		  
     		  //to reject negative number and odd number (condition for 1 question) 
-    		  //if((number<0) || (number % 2 != 0))
+    		  if((number<0) || (number % 2 != 0))
     		  {
     		 
     		  continue;
     		  }
+    		  
     		  sum = sum + number;
 
         	 
     	  }
     	  
       }while(number!=0);
+      
       System.out.println("Sum of Numbers : " + sum);
       sc.close();
 	}
